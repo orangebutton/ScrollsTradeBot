988ec1e9be05cec3c39256af8fd8c452c9a78613
Augustin Ku <augustin@cs.stanford.edu>
2014-03-15 20:48:21 -0400

    use a json config file, because the old password file wasn't working on windows.

---
+++ config.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"encoding/json"
+	"os"
+	"log"
+)
+
+//{
+//    "Email": "foo@foo.com",
+//    "Password": "mypassword",
+//    "UseWebserver": false,
+//    "Log": true
+//}
+type Config struct {
+    Email    	string
+    Password	string
+    UseWebserver bool
+    Log 	bool
+}
+
+func LoadConfig() *Config {	
+	file, _ := os.Open("conf.json")
+	decoder := json.NewDecoder(file)
+	config := new(Config)
+	decoder.Decode(config)
+	
+	log.Println("Email: ", config.Email) 
+	log.Println("Room: ", config.Email)
+	log.Println("UseWebserver: ", config.UseWebserver) 
+	log.Println("Log: ", config.Log)
+	
+	return config
+}
